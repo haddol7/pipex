50b244c2a80fe66a3d4134af52bf97a77156e085
haddol7 <haddol010313@khu.ac.kr>
2024-04-27 17:26:58 +0900

    [TEST] chaining pipe

--- src/test.c
+++ src/test.c
@@ -7,12 +7,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-void	child_proc(int *r_fd, int *w_fd, int n)
+void	child_proc(int *r_fd, int *w_fd, int n, int brother)
 {
 	char buf[100];
 	int	read_fd;
 	int	write_fd;
 
+	//open fd
 	if (n == 0)
 	{
 		read_fd = open("input.txt", O_RDONLY);
@@ -20,11 +21,15 @@ void	child_proc(int *r_fd, int *w_fd, int n)
 	}
 	else
 		read_fd = r_fd[0];
-	if (n == 2)
+	if (n == brother - 1)
+	{
 		write_fd = open("output.txt", O_WRONLY | O_CREAT, 777);
+		close(w_fd[1]);
+	}
 	else
 		write_fd = w_fd[1];
 	
+	//read & write
 	if (read(read_fd, buf, 100) == -1)
 	{
 		printf("errno - %d\n", errno);
@@ -34,48 +39,53 @@ void	child_proc(int *r_fd, int *w_fd, int n)
 	write(write_fd, buf, strlen(buf));
 	printf("%d-th child\n%s\n",n, buf);
 
-	if (!r_fd)
-		close(r_fd[1]);
-	if (!w_fd)
-		close(w_fd[0]);
+	//close fd
+	close(r_fd[1]);
+	close(w_fd[0]);
 	close(read_fd);
 	close(write_fd);
+	exit(0);
 }
 
 int	main(int argc, char *argv[])
 {
 	int fd[2];
 	int fd2[2];
+	int	pid;
+	int	n;
 
-	unlink("outfile");
+	n = 3;
+	unlink("output.txt");
 	pipe(fd);
 	pipe(fd2);
-	int pid = fork();
-	if (pid == 0)
-		child_proc(fd, fd2, 0);
-	else
-	{	
-		pipe(fd);
-		pid = fork();
-		if (pid == 0)
-			child_proc(fd2, fd, 1);
-		else
-		{	
-			pid = fork();
-			if (pid == 0)
-				child_proc(fd, NULL, 2);
-			else
+	for (int i = 0; i < n; i++)
+	{
+		if (i != 0)
+		{
+			if (i % 2 == 1)
 			{
-				wait(NULL);
-				wait(NULL);
-				wait(NULL);
 				close(fd[1]);
 				close(fd[0]);
+				pipe(fd);
+			}
+			else
+			{
 				close(fd2[1]);
 				close(fd2[0]);
-				printf("parent dead\n");
+				pipe(fd2);
 			}
 		}
+		pid = fork();
+		if (pid == 0 && i % 2 == 0)
+			child_proc(fd, fd2, i, n);
+		else if (pid == 0 && i % 2 == 1)
+			child_proc(fd2, fd, i, n);
 	}
-	
+	for(int i = 0; i < n; i++)
+		wait(NULL);
+	close(fd[1]);
+	close(fd[0]);
+	close(fd2[1]);
+	close(fd2[0]);
+	printf("parent dead\n");
 } 
